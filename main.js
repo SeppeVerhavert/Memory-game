e9fc649103ab26435485c1defff3747105c665e1
seppe_verhavert <seppe.verhavert@gmail.com>
2019-06-24 13:56:42 +0200

    identify cards

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
 const cards = document.querySelectorAll('.memoryCard');
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
 function flipCard() {
     this.classList.toggle('flip')
+
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+    }
+    else {
+        hasFlippedCard = false;
+        secondCard = this;
+
+        console.log(firstCard.dataset.name);
+        console.log(secondCard.dataset.name);
+    }
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
