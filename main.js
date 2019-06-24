610a2852f4e7ae580843e44b465b46643279b04c
seppe_verhavert <seppe.verhavert@gmail.com>
2019-06-24 14:37:37 +0200

    add shuffle

--- main.js
+++ main.js
@@ -1,23 +1,58 @@
 const cards = document.querySelectorAll('.memoryCard');
+window.addEventListener("load", shuffle);
 
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
 function flipCard() {
+    if (lockBoard) return;
     this.classList.toggle('flip')
 
     if (!hasFlippedCard) {
         hasFlippedCard = true;
         firstCard = this;
+        return;
+    }
+
+    hasFlippedCard = false;
+    secondCard = this;
+
+    checkForMatch();
+}
+
+function checkForMatch() {
+    if (firstCard.dataset.name === secondCard.dataset.name) {
+        disableCards();
     }
     else {
-        hasFlippedCard = false;
-        secondCard = this;
-
-        console.log(firstCard.dataset.name);
-        console.log(secondCard.dataset.name);
+        unflipCards();
     }
 }
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+}
+
+function unflipCards() {
+    lockBoard = true;
+
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+        lockBoard = false;
+    }, 1500);
+}
+
+function shuffle() {
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random()*12);
+        card.style.order = randomPos;
+    })
+    console.log("shuffled");
+}
+
 
 cards.forEach(card => card.addEventListener('click', flipCard));
 
